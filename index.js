2b983f45c797a7f41dde5997781446d2b6067a7a
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-03-01 17:54:29 -0500

    commit tree 68a2305713ffb6ed91c10dbb3741af293bea69d1

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callback) {
+  let newArray = [];
+  for(let i = 0; i < array.length; i++) {
+    newArray.push(callback(array[i]));
+  }
+  return newArray;
+}
+
+function reduce(array, callback, startingValue) {
+  let result = (!!startingValue) ? startingValue : array[0];
+    let i = (!!startingValue) ? 0 : 1
+
+  for(; i < array.length; i++) {
+    result = callback(array[i], result);
+  }
+  return result;
+}
